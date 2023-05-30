e5820b4f7bb8d6959b1decbb168797032ba66d7b
Graham Spencer <gspencer447@yahoo.com>
2023-05-30 14:47:16 -0500

    completed exercise 1

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-console.log("Hello World!\n==========\n");
+// console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+class Person {
+    constructor(name, pets, residence, hobbies) {
+      this.name = name;
+      this.pets = pets;
+      this.residence = residence;
+      this.hobbies = hobbies;
+    }
+    addHobby(hobby){
+      this.hobbies = this.hobbies + hobby;
+    }
+    removeHobby(hobby){
+      this.hobbies = this.hobbies - hobby;
+    }
+    greeting(){
+      console.log("Hello fellow person!");
+    }
+  }
+
+const person1 = new Person("Graham", 2, "alabama", [])
+console.log(person1);
+
+person1.greeting()
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
