a5c176e64b8a79d0a76303b51a2275fea6741cf5
Graham Spencer <gspencer447@yahoo.com>
2023-05-31 10:53:39 -0500

    uncommented exercises 1-3

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-// // console.log("Hello World!\n==========\n");
+// console.log("Hello World!\n==========\n");
 
-// // Exercise 1 Section
-// console.log("EXERCISE 1:\n==========\n");
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
 
-// class Person {
-//     constructor(name, pets, residence, hobbies) {
-//       this.name = name;
-//       this.pets = pets;
-//       this.residence = residence;
-//       this.hobbies = hobbies;
-//     }
-//     addHobby(hobby){
-//       this.hobbies = this.hobbies + hobby;
-//     }
-//     removeHobby(hobby){
-//       this.hobbies = this.hobbies - hobby;
-//     }
-//     greeting(){
-//       console.log("Hello fellow person!");
-//     }
-//   }
+class Person {
+    constructor(name, pets, residence, hobbies) {
+      this.name = name;
+      this.pets = pets;
+      this.residence = residence;
+      this.hobbies = hobbies;
+    }
+    addHobby(hobby){
+      this.hobbies = this.hobbies + hobby;
+    }
+    removeHobby(hobby){
+      this.hobbies = this.hobbies - hobby;
+    }
+    greeting(){
+      console.log("Hello fellow person!");
+    }
+  }
 
-// const person1 = new Person("Graham", 2, "alabama", [])
-// console.log(person1);
+const person1 = new Person("Graham", 2, "alabama", [])
+console.log(person1);
 
-// person1.greeting()
+person1.greeting()
 
-// // Exercise 2 Section
-// console.log("EXERCISE 2:\n==========\n");
-// class Coder extends Person{
-//   constructor(name, pets, residence, hobbies){
-//     super(name, pets, residence, hobbies)
-//     this.occupation = "Full Stack Web Developer";
-//   }
-//   greeting(){
-//     console.log("Hello from a coder!");
-//   }
-// }
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+class Coder extends Person{
+  constructor(name, pets, residence, hobbies){
+    super(name, pets, residence, hobbies)
+    this.occupation = "Full Stack Web Developer";
+  }
+  greeting(){
+    console.log("Hello from a coder!");
+  }
+}
 
-// const = coder1 new Coder("Larry", 4, "Mars", "drums");
-// console.log(coder1);
+const = coder1 new Coder("Larry", 4, "Mars", "drums");
+console.log(coder1);
 
-// //Exercise 3 Section
-// const person2 = new Person("Bob", 7, "Nebraska", "shuck corn");
-// const coder2 = new Coder ("Mark", 3, "Michigan", "football");
+//Exercise 3 Section
+const person2 = new Person("Bob", 7, "Nebraska", "shuck corn");
+const coder2 = new Coder ("Mark", 3, "Michigan", "football");
 
-// console.log(person2);
-// console.log(coder2);
+console.log(person2);
+console.log(coder2);
 
-//Exercise 4
+Exercise 4
 class Calculator {
   constructor(result = 0){
     this.result = result;
@@ -70,10 +70,6 @@ class Calculator {
   }
 
   divide(a, b = this.result){
-    // if (b === 0){
-    //   console.log("This number is not divisible by zero");
-    //   return this.result;
-    // }
     this.result = a / b;
     return this.result;
   }
